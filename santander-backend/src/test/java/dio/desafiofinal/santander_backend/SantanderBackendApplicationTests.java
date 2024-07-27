209fbab23c32f21146944d99d4d890679e722118
kevinkennedy00 <kevinkennedy.dev@gmail.com>
2024-07-27 16:13:16 -0300

    repositorio do desafio final.

---
+++ santander-backend/src/test/java/dio/desafiofinal/santander_backend/SantanderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafiofinal.santander_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
